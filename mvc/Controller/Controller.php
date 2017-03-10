e1e6cfc25b5f4d5a88c80757e8690565644e653f
Emaro <emaro@users.noreply.github.com>
2017-03-10 13:28:03 +0100

    Move getViewPath to View.php

--- mvc/Controller/Controller.php
+++ mvc/Controller/Controller.php
@@ -5,6 +5,9 @@
  * Date: 03.03.2017
  * Time: 10:53
  */
+
+require_once 'mvc/Controller/View.php'; // Relative to index.php
+
 class Controller {
     
     
@@ -16,7 +19,7 @@ class Controller {
     
     
     public function showView($uView) {
-    
+        
         /** @noinspection PhpUnusedLocalVariableInspection */
         $sView = htmlentities($uView); // Used to output the view name in views
         
@@ -31,18 +34,13 @@ class Controller {
     
     private function getValidViewPath($view) {
         
-        $path = $this->getViewPath($view);
+        $path = View::getViewPath($view);
         $viewExists = in_array($path, $this->views);
         
         if ($viewExists) {
             return $path;
         }
         
-        return $this->getViewPath(View::ERROR);
-    }
-    
-    private function getViewPath($view) {
-        
-        return View::LOCATION . $view . View::SUFFIX;
+        return View::get404Path();
     }
 }
--- mvc/Controller/View.php
+++ mvc/Controller/View.php
@@ -6,8 +6,20 @@
  * Time: 11:19
  */
 class View {
-    const SUFFIX = '.view.php';
-    const LOCATION = 'mvc/Views/';
-    const PATTERN = self::LOCATION . '*' . self::SUFFIX;
-    const ERROR = '404';
+    
+    const SUFFIX   = '.view.php';
+    const LOCATION = 'mvc/Views/'; // Relative to index.php
+    const PATTERN  = self::LOCATION . '*' . self::SUFFIX;
+    const ERROR    = '404';
+    
+    
+    public static function getViewPath($view) {
+        
+        return self::LOCATION . $view . self::SUFFIX;
+    }
+    
+    public static function get404Path() {
+        
+        return self::getViewPath(self::ERROR);
+    }
 }
