b697ab369fd7195207f03ab7512d4617b9418b84
Emaro <emaro@users.noreply.github.com>
2017-03-17 08:43:38 +0100

    Add a minimal menu

--- mvc/Controller/printLib.php
+++ mvc/Controller/printLib.php
@@ -5,29 +5,45 @@
  * Time: 13:55
  */
 
+/**
+ * @param string $title HTML title
+ */
 function printHead($title = 'PHP-MVC by DJM') {
     
     ?>
-    
+
     <!DOCTYPE html>
-    
+
     <head>
         <meta charset="UTF-8">
         <title><?=$title?></title>
     </head>
-    
+
     <body><code>body:</code>
+    <?php
+}
+
+function printMenu() {
+    
+    ?>
+
+    <nav><code>menu:</code>
+        <ul>
+            <li><a href="./?view=demo">Demo</a></li>
+            <li><a href="./?view=404">Error page</a></li>
+        </ul>
+    </nav>
     <?php
 }
 
 function printFoot() {
     
     ?>
-    
+
     <footer><code>footer:</code>
-        &#169; 2017 - David Schor, Joel Häberli, Miro Albrecht
+        <p>&#169; 2017 - David Schor, Joel Häberli, Miro Albrecht</p>
     </footer>
-    
+
     </body>
     <?php
 }
