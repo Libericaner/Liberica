b6fb2f170be93bb54e7e9136bbd1b4cdb3ace72e
Joel-Haeberli <haebu@rubigen.ch>
2017-03-17 08:49:18 +0100

    Init Model

---
+++ mvc/model/Picture.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * User: Joel Häberli
+ * Date: 17.03.2017
+ * Time: 08:44
+ */
+class Picture implements Queryable {
+    
+    //TODO : create query-pattern
+    
+    public function modelSelect(Integer $whichSelectStatement) {
+        // TODO: Implement modelSelect() method.
+    }
+    
+    public function modelInsert(Integer $whichInsertStatement) {
+        // TODO: Implement modelInsert() method.
+    }
+    
+    public function modelUpdate(Integer $whichUpdateStatement) {
+        // TODO: Implement modelUpdate() method.
+    }
+    
+    public function modelDelete(Integer $whichDeleteStatement) {
+        // TODO: Implement modelDelete() method.
+    }
+    
+    public function getQueryParameter() {
+        // TODO: Implement getQueryParameter() method.
+    }
+    
+    public function setQueryParameter(Array $params) {
+        // TODO: Implement setQueryParameter() method.
+    }
+}
