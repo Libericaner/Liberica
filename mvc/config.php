ab33c9c9a53fe27c6ece25e3ca15d5a0e6c7fa1e
Emaro <emaro@users.noreply.github.com>
2017-05-02 20:52:15 +0200

    Add settings file an extract some constants

--- mvc/config.php
+++ mvc/config.php
@@ -5,11 +5,10 @@
  * Time: 13:08
  */
 
-define("DEFAULT_PAGE", "login");
-define("ERROR_PAGE", "404");
-
-define("CSS_PATH", "main.css");
 
 define("USER", "user");
-
 define("TOKEN", "token");
+
+include "mvc/settings.php";
+
+// Define user files/folders, which shouldn't be updated
---
+++ mvc/settings.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User: Emaro
+ * Date: 2017-05-02
+ * Time: 20:39
+ */
+
+
+define("DEFAULT_PAGE", "login");
+define("ERROR_PAGE", "404");
+
+define("CSS_PATH", "main.css");
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "gibbiX12345");
+define("DB_NAME", "m151");
+
+// Not used yet
+define("GITHUB_OWNER", "Emaro");
+define("GITHUB_REPOSITORY", "Liberica");
